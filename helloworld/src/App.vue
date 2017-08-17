4898fb22396d2f6090300f52ba521baf99a8ca9b
hanmanjun <hanmanjun@molasync.com>
2017-08-17 10:30:27 +0800

    add demo 12

---
+++ helloworld/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">{{message}}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+#app{
+    font-size: 24px;
+  font-weight: bold;
+}
+</style>
